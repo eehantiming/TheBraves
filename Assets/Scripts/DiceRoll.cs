f8b239ca475dc4ae05ce081367f25e8495731532
Tjw <thorjw89@gmail.com>
2022-02-03 22:39:18 +0800

    Bug Fix - Dice Roll Works

--- Assets/Scripts/DiceRoll.cs
+++ Assets/Scripts/DiceRoll.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class DiceRoll : MonoBehaviour
 {
     public static DiceRoll Instance;
+    void Awake()
+    {
+        Instance = this;
+    }
     /// <summary>
     /// Generates a random number between 1 and 6
     /// </summary>
     public int Generate()
     {
-        int roll = Random.Range(1,6);
+        int roll = Random.Range(1,7);
         UIManager.Instance.ShowDiceRollText(roll);
         return (roll);
     }
