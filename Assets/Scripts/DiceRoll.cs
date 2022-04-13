7a617017d248977be6c7a02c1a65a753f10874f7
Tjw <thorjw89@gmail.com>
2022-04-13 23:05:18 +0800

    Dice working now

--- Assets/Scripts/DiceRoll.cs
+++ Assets/Scripts/DiceRoll.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class DiceRoll : MonoBehaviour
 {
+    public Sprite diceOne;
+    public Sprite diceTwo;
+    public Sprite diceThree;
+    public Sprite diceFour;
+    public Sprite diceFive;
+    public Sprite diceSix;
+
+    public SpriteRenderer spriteRenderer;
+
     public static DiceRoll Instance;
     void Awake()
     {
@@ -17,6 +26,37 @@ public class DiceRoll : MonoBehaviour
         int roll = Random.Range(1,7);
         UIManager.Instance.ShowDiceRollText(roll);
         Debug.Log("Roll: " + roll);
+        if(roll == 1)
+        {
+            ChangeSprite(diceOne);
+        }
+        else if(roll == 2)
+        {
+            ChangeSprite(diceTwo);
+        }
+        else if(roll == 3)
+        {
+            ChangeSprite(diceThree);
+        }
+        else if(roll == 4)
+        {
+            ChangeSprite(diceFour);
+        }
+        else if(roll == 5)
+        {
+            ChangeSprite(diceFive);
+        }
+        else if(roll == 6)
+        {
+            ChangeSprite(diceSix);
+        }
+
+
         return (roll);
+    }
+
+    public void ChangeSprite(Sprite newSprite)
+    {
+        spriteRenderer.sprite = newSprite;
     }
 }
